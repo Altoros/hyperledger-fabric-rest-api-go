be190f3f1f401bf98600bb4d5841bd574fef7aa4
Kochetkov Aleksandr <aleksandr.kochetkov@altoros.com>
2019-05-15 18:47:30 +0300

    BasicTestChaincode refactoring

--- test/chaincode/basic/main.go
+++ test/chaincode/basic/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	pb "github.com/hyperledger/fabric/protos/peer"
+	"strings"
 )
 
 type BasicTestChaincode struct {
@@ -13,18 +16,10 @@ type BasicTestChaincode struct {
 // This function is called only one when the chaincode is instantiated.
 // So the goal is to prepare the ledger to handle future requests.
 func (t *BasicTestChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
-	fmt.Println("########### BasicTestChaincode Init ###########")
-
-	// Get the function and arguments from the request
-	function, _ := stub.GetFunctionAndParameters()
-
-	// Check if the request is the init function
-	if function != "init" {
-		return shim.Error("Unknown function call")
-	}
+	fmt.Println("-=-=- BasicTestChaincode Init -=-=-")
 
 	// Put in the ledger the key/value hello/world
-	err := stub.PutState("hello", []byte("world"))
+	err := stub.PutState("value", []byte("Hello world!"))
 	if err != nil {
 		return shim.Error(err.Error())
 	}
@@ -33,103 +28,99 @@ func (t *BasicTestChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response 
 	return shim.Success(nil)
 }
 
-// Invoke
 // All future requests named invoke will arrive here.
 func (t *BasicTestChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-	fmt.Println("########### BasicTestChaincode Invoke ###########")
+	fmt.Println("-=-=- BasicTestChaincode Invoke -=-=-")
 
 	// Get the function and arguments from the request
 	function, args := stub.GetFunctionAndParameters()
 
-	// Check whether it is an invoke request
-	if function != "invoke" {
-		return shim.Error("Unknown function call")
-	}
-
 	// Check whether the number of arguments is sufficient
 	if len(args) < 1 {
-		return shim.Error("The number of arguments is insufficient.")
+		return shim.Error("The number of arguments is insufficient")
 	}
 
-	// In order to manage multiple type of request, we will check the first argument.
-	// Here we have one possible argument: query (every query request will read in the ledger without modification)
-	if args[0] == "query" {
+	switch function {
+	case "query":
 		return t.query(stub, args)
-	}
-
-	// The update argument will manage all update in the ledger
-	if args[0] == "invoke" {
-		return t.invoke(stub, args)
+	case "update":
+		return t.update(stub, args)
+	case "user":
+		return t.userInfo(stub)
 	}
 
 	// If the arguments given don’t match any function, we return an error
 	return shim.Error("Unknown action, check the first argument")
 }
 
-// query
 // Every readonly functions in the ledger will be here
 func (t *BasicTestChaincode) query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	fmt.Println("########### BasicTestChaincode query ###########")
+	fmt.Println("-=-=- BasicTestChaincode query -=-=-")
 
 	// Check whether the number of arguments is sufficient
-	if len(args) < 2 {
+	if len(args) < 1 {
 		return shim.Error("The number of arguments is insufficient.")
 	}
 
-	// Like the Invoke function, we manage multiple type of query requests with the second argument.
-	// We also have only one possible argument: hello
-	if args[1] == "hello" {
-
-		// Get the state of the value matching the key hello in the ledger
-		state, err := stub.GetState("hello")
-		if err != nil {
-			return shim.Error("Failed to get state of hello")
-		}
-
-		// Return this value in response
-		return shim.Success(state)
+	// Get the state of the value matching the key hello in the ledger
+	state, err := stub.GetState(args[0])
+	if err != nil {
+		return shim.Error("Failed to get state of value")
 	}
 
-	// If the arguments given don’t match any function, we return an error
-	return shim.Error("Unknown query action, check the second argument.")
+	// Return this value in response
+	return shim.Success(state)
 }
 
-// invoke
 // Every functions that read and write in the ledger will be here
-func (t *BasicTestChaincode) invoke(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	fmt.Println("########### BasicTestChaincode invoke ###########")
+func (t *BasicTestChaincode) update(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	fmt.Println("-=-=- BasicTestChaincode invoke -=-=-")
 
-	if len(args) < 2 {
-		return shim.Error("The number of arguments is insufficient.")
+	if len(args) != 2 {
+		return shim.Error("The number of arguments is incorrect, need 2 arguments.")
 	}
 
-	// Check if the ledger key is "hello" and process if it is the case. Otherwise it returns an error.
-	if args[1] == "hello" && len(args) == 3 {
-
-		// Write the new value in the ledger
-		err := stub.PutState("hello", []byte(args[2]))
-		if err != nil {
-			return shim.Error("Failed to update state of hello")
-		}
-
-		// Notify listeners that an event "eventInvoke" have been executed (check line 19 in the file invoke.go)
-		err = stub.SetEvent("eventInvoke", []byte("big payload"))
-		if err != nil {
-			return shim.Error(err.Error())
-		}
-
-		// Return this value in response
-		return shim.Success(nil)
+	// Write the new value in the ledger
+	err := stub.PutState(args[0], []byte(args[1]))
+	if err != nil {
+		return shim.Error("Failed to update state of hello")
 	}
 
-	// If the arguments given don’t match any function, we return an error
-	return shim.Error("Unknown invoke action, check the second argument.")
+	err = stub.SetEvent("eventInvoke", []byte("Big payload"))
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// Return this value in response
+	return shim.Success(nil)
+}
+
+// Fetch user info
+func (t *BasicTestChaincode) userInfo(stub shim.ChaincodeStubInterface) pb.Response {
+	creatorBytes, err := stub.GetCreator()
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	name, org := getCreator(creatorBytes)
+
+	// Return this value in response
+	return shim.Success([]byte(fmt.Sprintf("Name: %s, Org: %s, Pem: %s", name, org, creatorBytes)))
+}
+
+func getCreator(certificate []byte) (commonName, organization string) {
+	data := certificate[strings.Index(string(certificate), "-----") : strings.LastIndex(string(certificate), "-----")+5]
+	block, _ := pem.Decode(data)
+	cert, _ := x509.ParseCertificate(block.Bytes)
+	organization = cert.Issuer.Organization[0]
+	commonName = cert.Subject.CommonName
+	return
 }
 
 func main() {
 	// Start the chaincode and make it ready for futures requests
 	err := shim.Start(new(BasicTestChaincode))
 	if err != nil {
-		fmt.Printf("Error starting Heroes Service chaincode: %s", err)
+		fmt.Printf("Error starting chaincode: %s", err)
 	}
 }
